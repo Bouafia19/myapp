018d26a19ac664efecf9ce4fde578190a8293fc5
Bouafia19 <khaledsetif1900@gmail.com>
2021-08-31 22:09:31 +0100

    add footer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  def anglais
+  def index
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'welcome#anglais'
+  root 'welcome#index'
   resources :welcomes
-  get 'anglais', to: 'welcome#anglais'
+  get 'index', to: 'welcome#index'
   
 
 end
