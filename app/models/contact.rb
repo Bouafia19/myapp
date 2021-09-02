35ca1d64a9965e3191d9aa8fe2413ea458fff597
Bouafia19 <khaledsetif1900@gmail.com>
2021-09-02 16:49:35 +0100

    fix mail_form bugs

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,14 @@
 class Contact < MailForm::Base
     attributes :name, validate: true
-    attributes :email, validate: /\A[^@\s]+@[^@\s]+\z/i
-    attributes :message, validate: true
+    attributes :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+    attributes :message
+    attributes :subject
     attributes :nickname, captcha: true
 
     def headers
         {
           to: "khaledsetif1900@gmail.com",
-          subject: "Contact Form",
+          subject: "Form Subject",
           from: %("#{name}" <#{email}>) 
         }
       end
